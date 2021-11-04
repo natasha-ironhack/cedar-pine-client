ce749a1347ff56a4001394992802e93471550331
Natasha Smith <pisceanchic@gmail.com>
2021-11-04 20:30:34 +0100

    stuff done

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   state = {
     isLoggedIn: null,
     user: null,
+    cart: [],
     //need to check if user is able to edit or see page
     //so need to store user
   };
