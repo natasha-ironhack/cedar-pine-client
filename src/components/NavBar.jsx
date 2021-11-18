c77c327473f1061ca0699edb8cb57405771474fc
Natasha Smith <pisceanchic@gmail.com>
2021-11-18 15:29:33 +0100

    payment & cart icon # added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import authService from "../services/auth-service";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+// import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 // import IconButton from "@mui/icons-material/IconButton";
 // import Navbar from "react-bootstrap/Navbar"
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
 
 //passing props on line 6
 const NavBar = ({ isLoggedIn, user, setUser, cart }) => {
@@ -21,6 +27,8 @@ const NavBar = ({ isLoggedIn, user, setUser, cart }) => {
           0
         );
   };
+
+  const [itemCount, setItemCount] = React.useState(1);
 
   return (
     //     <Navbar bg="light" expand="lg">
@@ -88,10 +96,40 @@ const NavBar = ({ isLoggedIn, user, setUser, cart }) => {
           </li>
         </>
       )}
-      <NavLink to="/cart">Cart {calculateCartItems(cart)}</NavLink>
+      {/* <NavLink to="/cart">Cart {calculateCartItems(cart)}</NavLink> */}
       {/* <IconButton type="submit" color="success"> */}
-      <AddShoppingCartIcon></AddShoppingCartIcon>
+      {/* <AddShoppingCartIcon></AddShoppingCartIcon> */}
       {/* </IconButton> */}
+      <div style={{ display: "block", padding: 15 }}>
+        {/* <h4>How to create ShoppingCart Button in ReactJS?</h4> */}
+        <div>
+          <Badge color="secondary" badgeContent={calculateCartItems(cart)}>
+            {/* <ShoppingCartIcon />{" "} */}
+            <NavLink to="/cart">
+              {" "}
+              <ShoppingCartIcon />{" "}
+            </NavLink>
+          </Badge>
+          {/* <ButtonGroup>
+            <Button
+              onClick={() => {
+                // setItemCount(Math.max(itemCount - 1, 0));
+              }}
+            >
+              {" "}
+              <RemoveIcon fontSize="small" />
+            </Button>
+            <Button
+              onClick={() => {
+                // setItemCount(itemCount + 1);
+              }}
+            >
+              {" "}
+              <AddIcon fontSize="small" />
+            </Button>
+          </ButtonGroup> */}
+        </div>
+      </div>
     </ul>
   );
 };
